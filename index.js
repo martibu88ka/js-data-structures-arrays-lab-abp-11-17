5dfe463cd52b735ce5816d4afd590ecde5883704
Martyna <martyna.glaz@gmail.com>
2019-04-28 17:34:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,6 @@ function destructivelyRemoveLastDriver(){
 function destructivelyRemoveFirstDriver(){
   drivers.shift(1);
 }
+function appendDriver(Broom){
+  const addDriver = [...drivers, "Broom"]
+}
