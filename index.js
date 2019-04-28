54f473fa231d2120ebf00e76af9461beddf33fd5
Martyna <martyna.glaz@gmail.com>
2019-04-28 17:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendDriver(Ralph){
+drivers.splice(3,0,"Ralph");
+}
+function destructivelyPrependDriver(Bob){
+  drivers.splice(0,0,"Bob");
+}
+function destructivelyRemoveLastDriver(){
+  drivers.splice(-1);
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.slice(1);
+}
